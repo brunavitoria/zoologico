43bbd3ca1e96b5dc207246ea42266040c9e1b0eb
Bruna Vitória <brunavitoriati@gmail.com>
2024-06-29 17:59:01 -0300

    (feat): Criando rotas com annotations

--- src/Controller/MamiferoController.php
+++ src/Controller/MamiferoController.php
@@ -3,11 +3,25 @@
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class MamiferoController
 {
+    /**
+     * @Route("/")
+     */
     public function homepage()
     {
         return new Response('Olá, mamíferos!');
+    }
+
+    /**
+     * @Route("/mamifero/{slug}")
+     */
+    public function show($slug)
+    {
+        return new Response(
+            sprintf('<html><body>Esta é a página de um mamífero "%s"!</body></html>', ucwords(str_replace('-', ' ', $slug)))
+        );
     }
 }
